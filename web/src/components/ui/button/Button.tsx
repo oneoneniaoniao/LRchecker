d8ade7243779fef0a2e94df51cb13c2ba14f139e
maru <321marumaru123@gmail.com>
2025-12-13 16:56:37 +0900

    tweak

--- web/src/components/ui/button/Button.tsx
+++ web/src/components/ui/button/Button.tsx
@@ -18,7 +18,7 @@ const Button: React.FC<ButtonProps> = ({
       className={`bg-blue-500 text-white font-semibold px-8 py-4 rounded-xl shadow-lg transform transition-all duration-200 min-w-[200px] text-2xl ${
         disabled
           ? "opacity-50 cursor-not-allowed"
-          : "hover:bg-blue-600 hover:shadow-xl hover:-translate-y-1 cursor-pointer active:scale-95"
+          : "hover:shadow-xl hover:-translate-y-1 cursor-pointer active:scale-95"
       }`}
     >
       {buttonText}
